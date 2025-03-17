05d31cb174b8269e86df05f74912a47f2a7b1e16
drew <drew.c.pierson@gmail.com>
2025-03-17 14:10:54 -0500

    Add TimeoutRef.js

---
+++ The Complete Full-Stack Web Development Bootcamp/js/Simon Game Challenge Starting Files/js/TimoutRef.js
@@ -0,0 +1,11 @@
+/**
+ * Class for passing ref for timeout.
+ */
+export class TimeoutRef {
+    /**
+    * @param {Number} timeout - Number indicating time between beeps.
+    */
+    constructor(timeout) {
+        this.timeout = timeout;
+    }
+}
