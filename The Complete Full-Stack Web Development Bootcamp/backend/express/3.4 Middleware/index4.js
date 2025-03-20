d29c79856b6aa5451db295ad33ea367a9f369529
drew <drew.c.pierson@gmail.com>
2025-03-19 19:18:47 -0500

    Complete 3.4

--- The Complete Full-Stack Web Development Bootcamp/backend/express/3.4 Middleware/index4.js
+++ The Complete Full-Stack Web Development Bootcamp/backend/express/3.4 Middleware/index4.js
@@ -1,7 +1,25 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { dirname, parse } from "path";
+import { fileURLToPath } from "url";
+
+const filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(filename);
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send();
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
