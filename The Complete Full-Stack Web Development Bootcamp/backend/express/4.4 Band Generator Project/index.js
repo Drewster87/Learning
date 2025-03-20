38405db88f543301d90e7fb29c74f2a444406be5
drew <drew.c.pierson@gmail.com>
2025-03-20 14:02:16 -0500

    Band Generator done

--- The Complete Full-Stack Web Development Bootcamp/backend/express/4.4 Band Generator Project/index.js
+++ The Complete Full-Stack Web Development Bootcamp/backend/express/4.4 Band Generator Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
@@ -12,20 +13,23 @@ const port = 3000;
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
 
+app.use(morgan("combined"));
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
+  res.render("index.ejs", {
+    bandname:
+      adj[Math.floor(Math.random() * adj.length)] +
+      " " +
+      noun[Math.floor(Math.random() * noun.length)],
+  });
 });
 
 app.listen(port, () => {
